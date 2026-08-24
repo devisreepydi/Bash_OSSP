a6b1e506a804463f490ad890fea9b52397eb18ca
KALLU SHRESTA PRIYA <kallushrestapriya@klh.edu.in>
2026-08-24 23:50:52 +0530

    Update s2.c

--- 2520030127_Skill/OSSP_Skill 2 /s2.c
+++ 2520030127_Skill/OSSP_Skill 2 /s2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    char command[100];
+
+    while (1)
+    {
+        printf("myshell> ");
+
+        if (fgets(command, sizeof(command), stdin) == NULL)
+        {
+            break;
+        }
+
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strcmp(command, "exit") == 0)
+        {
+            break;
+        }
+
+        printf("You entered: %s\n", command);
+    }
+
+    return 0;
+}
