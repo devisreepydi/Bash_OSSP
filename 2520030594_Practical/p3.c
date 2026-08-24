61b51c679abf72f822025bebde7700bb3c22e1da
devisreepydi <devisreepydi@gmail.com>
2026-08-24 20:25:34 +0530

    Create p3.c

---
+++ 2520030594_Practical/p3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main()
+{
+int pid;
+pid = fork();
+if (pid == 0)
+{
+printf("Child Process\n");
+printf("PID  = %d\n", getpid());
+printf("PPID = %d\n", getppid());
+printf("State = Running\n");
+}
+else
+{
+printf("Parent Process\n");
+printf("PID  = %d\n", getpid());
+printf("PPID = %d\n", getppid());
+printf("State = Running\n");
+wait(NULL);
+printf("Parent: Child process completed\n");
+}
+return 0;
+}
