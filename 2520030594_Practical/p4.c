1bda4e9ec8db27ac084e4dba25e9951404bf994f
devisreepydi <devisreepydi@gmail.com>
2026-09-19 14:42:51 +0530

    Create p4.c

---
+++ 2520030594_Practical/p4.c
@@ -0,0 +1,45 @@
+                                            waitex.c
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+int main(){
+int pid[3];
+for(int i=0;i<3;i++)
+{
+pid[i]=fork();
+if(pid[i]==0)
+{
+printf("Child %d pid=%d\n",i+1,getpid());
+sleep(i+1);
+exit(0);
+}
+}
+wait(NULL);
+printf("Parent:Any one child completes with wait()\n");
+waitpid(pid[2],NULL,0);
+printf("Parent:Chlid 3 completes with waitpid()\n");
+wait(NULL);
+printf("Parent:All the childs completed");
+}
+  
+                                               zombie.c
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+{
+int pid;
+pid=fork();
+if(pid==0)
+{
+printf("Chlid process%d\n",getpid());
+exit(0);
+}
+else{
+printf("Parent process %d\n",getpid());
+sleep(30);
+//wait(NULL);
+printf("Parent process completed");
+}
+}
